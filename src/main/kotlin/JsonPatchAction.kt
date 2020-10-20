0dbda66fa516f3c6ee092abf343752871c1fbf40
smyachenkov <s.myachenkov@gmail.com>
2020-10-20 21:58:19 +0300

    add input validation

--- src/main/kotlin/JsonPatchAction.kt
+++ src/main/kotlin/JsonPatchAction.kt
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.JsonMappingException
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.flipkart.zjsonpatch.JsonPatch
 import com.flipkart.zjsonpatch.JsonPatchApplicationException
@@ -9,8 +10,12 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.ide.CopyPasteManager
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.InputValidator
 import com.intellij.openapi.ui.Messages
+import java.awt.datatransfer.DataFlavor
+import java.awt.datatransfer.Transferable
 
 class JsonPatchAction : AnAction() {
 
@@ -20,50 +25,45 @@ class JsonPatchAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         val psiFile = e.getData(CommonDataKeys.PSI_FILE)!!
-        val originalContent = mapper.readTree(psiFile.text)
+        if (psiFile.text.isEmpty()) {
+            Messages.showErrorDialog(
+                "Can't apply patch to empty document",
+                "JSON must be valid"
+            )
+            return
+        }
+        val originalContent: JsonNode
+        try {
+            originalContent = mapper.readTree(psiFile.text)
+        } catch (e: JsonProcessingException) {
+            Messages.showErrorDialog(
+                "Can't convert this file to JSON document: ${e.originalMessage}",
+                "JSON must be valid"
+            )
+            return
+        }
 
-        val jsonPatchInput = Messages.showMultilineInputDialog(
-            e.project,
-            "Enter JSON Patch value",
-            "JSON Patch",
-            "",
-            Messages.getInformationIcon(),
-            object : InputValidator {
-                override fun checkInput(inputString: String?): Boolean {
-                    if (inputString == null || inputString.isBlank()) {
-                        return false
-                    }
-                    try {
-                        val jsonPatch = mapper.readTree(inputString)
-                        JsonPatch.apply(jsonPatch, originalContent)
-                    } catch (e: Exception) {
-                        when (e) {
-                            is JsonMappingException, is JsonProcessingException -> {
-                                log.info("Error converting input to JSON", e)
-                            }
-                            is JsonPatchApplicationException -> {
-                                log.info("Can't apply this patch", e)
-                            }
-                            else -> {
-                                log.info("Unexpected error", e)
-                            }
-                        }
-                        return false
-                    }
-                    return true
-                }
+        val clipboard = CopyPasteManager.getInstance().allContents
 
-                override fun canClose(inputString: String?): Boolean {
-                    return true
-                }
-            }
-        )
+        val jsonPatchInput = if (clipboardHasValidPatch(clipboard, originalContent)) {
+            clipboard[0].getTransferData(DataFlavor.stringFlavor) as String
+        } else {
+            getPatchFromDialog(e.project!!, originalContent)
+        }
+
         val jsonPatch = mapper.readTree(jsonPatchInput)
         val patchedContent = JsonPatch.apply(jsonPatch, originalContent)
         val editor = e.getData(CommonDataKeys.EDITOR)
         val document = editor!!.document
+
+        log.debug("Replacing content of file ${psiFile.name} with new content: ${patchedContent.toPrettyString()}")
+
         runWriteAction {
-            document.setText(patchedContent.toPrettyString().replace("\r\n", "\n"))
+            document.setText(
+                patchedContent
+                    .toPrettyString()
+                    .replace("\r\n", "\n")
+            )
         }
     }
 
@@ -78,4 +78,48 @@ class JsonPatchAction : AnAction() {
         super.update(e)
     }
 
+    private fun clipboardHasValidPatch(clipboard: Array<Transferable>, originalContent: JsonNode): Boolean {
+        if (clipboard.isEmpty()) {
+            return false
+        }
+        val latestEntry = clipboard[0].getTransferData(DataFlavor.stringFlavor) as String
+        return latestEntry.isValidJsonPatch(originalContent)
+    }
+
+    private fun getPatchFromDialog(project: Project, originalContent: JsonNode) =
+        Messages.showMultilineInputDialog(
+            project,
+            "Enter JSON Patch value. It must be valid, or else it cannot be applied",
+            "JSON Patch",
+            "[\n]",
+            Messages.getInformationIcon(),
+            object : InputValidator {
+                override fun checkInput(inputString: String?) = inputString.isValidJsonPatch(originalContent)
+                override fun canClose(inputString: String?): Boolean = true
+            }
+        )
+
+    private fun String?.isValidJsonPatch(originalContent: JsonNode): Boolean {
+        if (this == null || this.isBlank()) {
+            return false
+        }
+        try {
+            val jsonPatch = mapper.readTree(this)
+            JsonPatch.apply(jsonPatch, originalContent)
+        } catch (e: Exception) {
+            when (e) {
+                is JsonMappingException, is JsonProcessingException -> {
+                    log.info("Error converting input to JSON", e)
+                }
+                is JsonPatchApplicationException -> {
+                    log.info("Can't apply this patch", e)
+                }
+                else -> {
+                    log.info("Unexpected error", e)
+                }
+            }
+            return false
+        }
+        return true
+    }
 }
